d9f01fbc2b3f13bc04d7784b361a2e8b288d6251
Russell Vaughan <russell@gosquared.com>
2016-06-23 20:01:05 +0100

    wip building trends class

--- lib/go_squared.rb
+++ lib/go_squared.rb
@@ -1,29 +1,40 @@
 class GoSquared 
 
 	DIMENSIONS = %w(aggregate browser category country event language organisation os page path1 product screen_dimensions sources transactions)
+	VERSION = %w(v1 v2 v3)
 
-		DIMENSIONS.each do |dimension|
-			define_method dimension do
-				@dimension = dimension + "?"
-				self
-			end
+	
+	def initialize(api_key="demo", site_token="GSN-181546-E")
+		@site_token = site_token
+		@api_key = api_key
+	end
+
+	DIMENSIONS.each do |dimension|
+		define_method dimension do
+			@dimension = dimension + "?"
+			self
 		end
+	end	
+
+	VERSION.each do |version|
+		define_method version do
+			@version = version + "/"
+			self
+		end
+	end
 
 	def trends
 		@trends = 'trends/'
 		self
 	end
 
-	def v2
-		@v2 = 'v2/'
-		self
-	end
-
 	def fetch
-		url =  "https://api.gosquared.com/"+@trends+@v2+@dimension
+		url =  "https://api.gosquared.com/"+@trends+@version+@dimension+"api_key=demo&site_token=GSN-106863-S"
 		puts "#{url}"
 		uri = URI(url)
 		response = Net::HTTP.get(uri)
 	end
+
+
 
 end
--- spec/go_squared_spec.rb
+++ spec/go_squared_spec.rb
@@ -2,16 +2,20 @@ describe GoSquared do
 	subject(:gs) { described_class.new() }
 
 	DIMENSIONS = %w(aggregate browser category country event language organisation os page path1 product screen_dimensions sources transactions)
+	VERSION = %w(v1 v2 v3)
 
- 	DIMENSIONS.each do |dimension|
- 	it { is_expected.to respond_to(dimension) }  
- 	end
+	DIMENSIONS.each do |dimension|
+		it { is_expected.to respond_to(dimension) }  
+	end
 
-	
+	VERSION.each do |version|
+		it { is_expected.to respond_to(version) }  
+	end
+
 
 	before(:each) do
 		data = 'test'
-		stub_request(:get, 'https://api.gosquared.com/trends/v2/aggregate').
+		stub_request(:get, 'https://api.gosquared.com/trends/v2/aggregate?api_key=demo&site_token=GSN-106863-S').
 		with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
 		to_return(status: 200, body: data, headers: {})
 	end
@@ -19,6 +23,14 @@ describe GoSquared do
 	it 'fetches a request from the GoSquared Trends API'do
 	gs.trends.v2.aggregate
 	expect(gs.fetch).to eq('test')
-end
+	end
+
+	it 'fetches a request from the GoSquared Trends API with paramaters'do
+	gs.trends.v2.aggregate.from('2016-06-20').to('2016-06-30')
+	.dateFormat('yyyy-mm-dd').sort('field').group(true)
+	expect(gs.fetch).to eq('params_test')
+	end
+
+	# curl "https://api.gosquared.com/trends/v2/browser?api_key=demo&site_token=GSN-106863-S&from='2016-06-20'&to='2016-06-30'&format=json&limit=5&group=true" 
 
 end
