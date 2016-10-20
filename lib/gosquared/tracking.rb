e8fa674d8f9c2044eb870429b4a0e8598b8ec3ac
Russell Vaughan <russell@gosquared.com>
2016-10-20 18:13:39 +0100

    Add error handling for when person_id is nil in Event post request -add validation check in #post method in tracking.rb -add test to tracking_spec to test for successful post when person_id = nil

--- lib/gosquared/tracking.rb
+++ lib/gosquared/tracking.rb
@@ -1,31 +1,41 @@
 require_relative "client"
-	class Tracking
+class Tracking
 
-		BASEURL = "https://api.gosquared.com/tracking/v1/"
-		DIMENSIONS = %w(event identify pageview ping properties timeout transaction)
+	BASEURL = "https://api.gosquared.com/tracking/v1/"
+	DIMENSIONS = %w(event identify pageview ping properties timeout transaction)
 
-		def initialize(api_key, site_token, client=Client.new)
-			@site_token = site_token
-			@api_key = api_key
-			@client = client
-		end
-
-		DIMENSIONS.each do |dimension|
-			define_method dimension do |options|
-				@dimension = dimension
-				@data = options
-				self
-			end
-		end
-
-		def post
-			response = @client.post(url, @data)
-			@data = nil if response.code === '200'
-			response
-		end
-
-		def url
-			url = BASEURL + @dimension + "?api_key=#{@api_key}" + "&site_token=#{@site_token}"
-		end
-
+	def initialize(api_key, site_token, client=Client.new)
+		@site_token = site_token
+		@api_key = api_key
+		@client = client
 	end
+
+	DIMENSIONS.each do |dimension|
+		define_method dimension do |options|
+			@dimension = dimension
+			@data = options
+			self
+		end
+	end
+
+	def post
+		check_for_nil_user
+		response = @client.post(url, @data)
+		@data = nil if response.code === '200'
+		response
+	end
+
+	def url
+		url = BASEURL + @dimension + "?api_key=#{@api_key}" + "&site_token=#{@site_token}"
+	end
+
+	private
+
+	def check_for_nil_user
+		if @data.key?(:person_id) && @data[:person_id] == nil
+			@data.tap { |data| @data.delete(:person_id) }
+			warn 'person_id is nil, event will not be track against a user'
+		end
+	end
+
+end
--- spec/tracking_spec.rb
+++ spec/tracking_spec.rb
@@ -1,21 +1,50 @@
-describe Tracking do 
+describe Tracking do
 	subject(:gs) { described_class.new("demo", "GSN-2194840-F") }
 
-	Tracking::DIMENSIONS.each do |dimension|	
-	before do 
+	Tracking::DIMENSIONS.each do |dimension|
+		before do
 			stub_request(:post, "https://api.gosquared.com/tracking/v1/#{dimension}?api_key=demo&site_token=GSN-2194840-F").
 			with(:body => "[ {\"person_id\":\"email:test@example.com\",\"properties\":{\"email\":\"test@example.com\"}} ]",
-				:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).	
+				:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).
 			to_return(:status => 200, :body => "", :headers => {response: "success"})
 		end
 	end
 
-	Tracking::DIMENSIONS.each do |dimension|	
+	Tracking::DIMENSIONS.each do |dimension|
 		it "posts a request to the GoSquared Track API with #{dimension} dimension" do
-			gs.send "#{dimension}", ({person_id: "email:test@example.com", 
+			gs.send "#{dimension}", ({person_id: "email:test@example.com",
 				properties:{email: "test@example.com"}})
 			expect(gs.post.code).to eq('200')
 		end
+	end
+
+	describe "#event" do
+		before do
+			stub_request(:post, "https://api.gosquared.com/tracking/v1/event?api_key=demo&site_token=GSN-2194840-F").
+			with(:body => "[ {\"event\":{\"name\":\"event\"}} ]",
+				:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).
+			to_return(:status => 200, :body => "", :headers => {})
+			@data = {person_id: nil,  event: { name: 'event' } }
+		end
+
+		it 'Tracks an anonymous event when a person_id is nil' do
+			gs.event(@data)
+			expect(gs.post.code).to eq('200')
+		end
+
+		before do
+			stub_request(:post, "https://api.gosquared.com/tracking/v1/event?api_key=demo&site_token=GSN-2194840-F").
+			with(:body => "[ {\"person_id\":\"12345\",\"event\":{\"name\":\"event\"}} ]",
+				:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).
+			to_return(:status => 200, :body => "", :headers => {})
+			@data = {person_id: '12345',  event: { name: 'event' } }
+		end
+
+		it 'Tracks an anonymous event when a person_id is not nil' do
+			gs.event(@data)
+			expect(gs.post.code).to eq('200')
+		end
+
 	end
 
 end
