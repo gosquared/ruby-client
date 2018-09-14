e5d7266d614d142fc7f5df6536b2ed3056abd6c8
Adrian Marin <adrian@adrianthedev.com>
2018-09-14 13:52:54 +0200

    Update tracking_spec.rb

--- spec/tracking_spec.rb
+++ spec/tracking_spec.rb
@@ -6,7 +6,7 @@ describe Gosquared::Tracking do
 	Gosquared::Tracking::DIMENSIONS.each do |dimension|
 		before do
 			stub_request(:post, "https://api.gosquared.com/tracking/v1/#{dimension}?api_key=demo&site_token=GSN-2194840-F").
-			with(:body => "[ {\"person_id\":\"email:test@example.com\",\"properties\":{\"email\":\"test@example.com\"}} ]",
+			with(:body => "[{\"person_id\":\"email:test@example.com\",\"properties\":{\"email\":\"test@example.com\"}}]",
 				:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'ruby-client/'+VERSION }).
 			to_return(:status => 200, :body => "", :headers => {response: "success"})
 		end
@@ -23,7 +23,7 @@ describe Gosquared::Tracking do
 	describe "#event" do
 		before do
 			stub_request(:post, "https://api.gosquared.com/tracking/v1/event?api_key=demo&site_token=GSN-2194840-F").
-			with(:body => "[ {\"event\":{\"name\":\"event\"}} ]",
+			with(:body => "[{\"event\":{\"name\":\"event\"}}]",
 				:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'ruby-client/'+VERSION}).
 			to_return(:status => 200, :body => "", :headers => {})
 			@data = {person_id: nil,  event: { name: 'event' } }
@@ -36,7 +36,7 @@ describe Gosquared::Tracking do
 
 		before do
 			stub_request(:post, "https://api.gosquared.com/tracking/v1/event?api_key=demo&site_token=GSN-2194840-F").
-			with(:body => "[ {\"person_id\":\"12345\",\"event\":{\"name\":\"event\"}} ]",
+			with(:body => "[{\"person_id\":\"12345\",\"event\":{\"name\":\"event\"}}]",
 				:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'ruby-client/'+VERSION}).
 			to_return(:status => 200, :body => "", :headers => {})
 			@data = {person_id: '12345',  event: { name: 'event' } }
